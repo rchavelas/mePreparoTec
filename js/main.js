0c686e7d6b44a8f76e0770dc498af79666b47e7a
Ricardo Chavelas <69439761+rchavelas@users.noreply.github.com>
2021-04-20 17:41:12 -0700

    se agregan colores por tipo de evento y listas personalizadas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict'
 // Elementos globales
 var sidebarList = document.getElementById("sidebarList");
+var sidebarListOblig = document.getElementById("sidebarListOblig");
+var sidebarListLife = document.getElementById("sidebarListLife");
+
+
 var credsSeleccionados = 0;
 
 /** WA */
@@ -57,7 +61,7 @@ function agregarWA1057(params) {
     let liWA1057 = document.createElement("li")
     liWA1057.id = "liWA1057"
     liWA1057.innerHTML = "0 UdC | WA1057: Mi plan de Vida en el Tec"
-    sidebarList.appendChild(liWA1057);
+    sidebarListOblig.appendChild(liWA1057);
 }
 function quitarWA1057(params) {
     let liWA1057 = document.getElementById("liWA1057")
@@ -84,7 +88,7 @@ EG1P1.addEventListener("click",function(){
         EG1P3.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();
-        addToList("EG1","3 UdC | Optativa de Educación General")     
+        addToList("EG1","3 UdC | Materia de Educación General")     
     };
 });
 
@@ -102,7 +106,7 @@ EG1P2.addEventListener("click",function(){
         EG1P3.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();
-        addToList("EG1","3 UdC | Optativa de Educación General")     
+        addToList("EG1","3 UdC | Materia de Educación General")     
     };
 })
 
@@ -120,7 +124,7 @@ EG1P3.addEventListener("click",function(){
         EG1P3.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();
-        addToList("EG1","3 UdC | Optativa de Educación General")     
+        addToList("EG1","3 UdC | Materia de Educación General")     
     };
 })
 
@@ -165,7 +169,7 @@ function agregarST1(params) {
     let liST1 = document.createElement("li")
     liST1.id = "liST1"
     liST1.innerHTML = "0 UdC | Semana 6"
-    sidebarList.appendChild(liST1);
+    sidebarListOblig.appendChild(liST1);
 }
 function quitarST1(params) {
     let liST1 = document.getElementById("liST1")
@@ -177,7 +181,7 @@ function agregarST2(params) {
     let liST2 = document.createElement("li")
     liST2.id = "liST2"
     liST2.innerHTML = "0 UdC | Semana 12"
-    sidebarList.appendChild(liST2);
+    sidebarListOblig.appendChild(liST2);
 }
 function quitarST2(params) {
     let liST2 = document.getElementById("liST2")
@@ -189,7 +193,7 @@ function agregarST3(params) {
     let liST3 = document.createElement("li")
     liST3.id = "liST3"
     liST3.innerHTML = "0 UdC | Semana 18"
-    sidebarList.appendChild(liST3);
+    sidebarListOblig.appendChild(liST3);
 }
 function quitarST3(params) {
     let liST3 = document.getElementById("liST3")
@@ -221,7 +225,7 @@ B1.addEventListener("click",function(){
         B1.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();   
-        addToList("B1","3 UdC | Bloque 1")     
+        addToList("B1","3 UdC | F1001B: Modelación de la ingeniería y ciencias")     
     };
 })
 
@@ -236,7 +240,7 @@ B2.addEventListener("click",function(){
         B2.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();
-        addToList("B2","3 UdC | Bloque 2")     
+        addToList("B2","3 UdC | F1006B: Modelación del movimiento en ingeniería")     
     };
 });
 
@@ -251,39 +255,43 @@ B3.addEventListener("click",function(){
         B3.classList.add("active");
         credsSeleccionados+=3;
         actualizarDom();
-        addToList("B3","3 UdC | Bloque 3");     
+        addToList("B3","3 UdC | F1007B: Aplicación de las leyes de conservación en sistemas ingenieriles");     
     }
 })
 
-/** M */
+/** Materias */
+// Materia 1 periodo 1 (y 2, se quita el id M1P2 de la lista de selección)
 let M1P1 = document.getElementById("M1P1")
 M1P1.addEventListener("click",function(){
     if(M1P1.classList.contains("active")){
-        M1P1.classList.remove("active");
-        credsSeleccionados-=1;
+        M1P2.classList.remove("active","tenWk");
+        M1P1.classList.remove("active","tenWk");
+        credsSeleccionados-=2;
         actualizarDom();
         removeFromList("M1P1")
     } else {
-        M1P1.classList.add("active");
-        credsSeleccionados+=1;
+        M1P1.classList.add("active","tenWk");
+        M1P2.classList.add("active","tenWk");
+        credsSeleccionados+=2;
         actualizarDom();
-        addToList("M1P1","1 UdC | M1P1")     
+        addToList("M1P1","2 UdC | TC1028: Pensamiento computacional para ingeniería")     
     }
 });
 
 let M1P2 = document.getElementById("M1P2")
 M1P2.addEventListener("click",function(){
     if(M1P2.classList.contains("active")){
-        M1P2.classList.remove("active");
-        credsSeleccionados-=1;
+        M1P2.classList.remove("active","tenWk");
+        M1P1.classList.remove("active","tenWk");
+        credsSeleccionados-=2;
         actualizarDom();
-        removeFromList("M1P2")
+        removeFromList("M1P1")
     } else {
-        M1P2.classList.add("active");
-        credsSeleccionados+=1;
+        M1P2.classList.add("active","tenWk");
+        M1P1.classList.add("active","tenWk");
+        credsSeleccionados+=2;
         actualizarDom();
-        addToList("M1P2","1 UdC | M1P2")     
-
+        addToList("M1P1","2 UdC | TC1028: Pensamiento computacional para ingeniería")     
     }
 });
 
@@ -298,7 +306,7 @@ M1P3.addEventListener("click",function(){
         M1P3.classList.add("active");
         credsSeleccionados+=1;
         actualizarDom();
-        addToList("M1P3","1 UdC | M1P3")     
+        addToList("M1P3","1 UdC | Q1029: Análisis de la estructura y transfromación de la materia")     
 
     }
 });
@@ -314,37 +322,42 @@ M2P1.addEventListener("click",function(){
         M2P1.classList.add("active");
         credsSeleccionados+=1;
         actualizarDom();
-        addToList("M2P1","1 UdC | M2P1")     
+        addToList("M2P1","1 UdC | Q1028: Fundamentación de la estructura y transformación de la materia")     
     }
 });
 
+// M2 del periodo 2 y 3 se cambia por una sola de 10 semanas
 let M2P2 = document.getElementById("M2P2")
 M2P2.addEventListener("click",function(){
     if(M2P2.classList.contains("active")){
-        M2P2.classList.remove("active");
-        credsSeleccionados-=1;
+        M2P2.classList.remove("active","tenWk");
+        M2P3.classList.remove("active","tenWk");
+        credsSeleccionados-=2;
         actualizarDom();
         removeFromList("M2P2")
     } else {
-        M2P2.classList.add("active");
-        credsSeleccionados+=1;
+        M2P2.classList.add("active","tenWk");
+        M2P3.classList.add("active","tenWk");
+        credsSeleccionados+=2;
         actualizarDom();
-        addToList("M2P2","1 UdC | M2P2")     
+        addToList("M2P2","2 UdC | MA1028: Modelación matemática fundamental")     
     }
 });
 
 let M2P3 = document.getElementById("M2P3")
 M2P3.addEventListener("click",function(){
     if(M2P3.classList.contains("active")){
-        M2P3.classList.remove("active");
-        credsSeleccionados-=1;
+        M2P3.classList.remove("active","tenWk");
+        M2P2.classList.remove("active","tenWk");
+        credsSeleccionados-=2;
         actualizarDom();
-        removeFromList("M2P3")
+        removeFromList("M2P2")
     } else {
-        M2P3.classList.add("active");
-        credsSeleccionados+=1;
+        M2P3.classList.add("active","tenWk");
+        M2P2.classList.add("active","tenWk");
+        credsSeleccionados+=2;
         actualizarDom();
-        addToList("M2P3","1 UdC | M2P3")     
+        addToList("M2P2","2 UdC | MA1028: Modelación matemática fundamental")     
     }
 });
 
@@ -358,43 +371,43 @@ actualizarDom()
 // Tomar UFs life y colocarlas en la lista
 let inputLife1 = document.getElementById("inputLife1")
 inputLife1.addEventListener("focusout",function(){
-    let hasChild = sidebarList.querySelector("#inputLife1_lielem");
+    let hasChild = sidebarListLife.querySelector("#inputLife1_lielem");
     if(hasChild != null){
-        sidebarList.removeChild(hasChild)
+        sidebarListLife.removeChild(hasChild)
     }   
     if(inputLife1.value !== ""){
         let liElement = document.createElement("li")
         liElement.innerText = "LiFE | "+ inputLife1.value
         liElement.id = "inputLife1_lielem";
-        sidebarList.appendChild(liElement);
+        sidebarListLife.appendChild(liElement);
     } 
 });
 
 let inputLife2 = document.getElementById("inputLife2")
 inputLife2.addEventListener("focusout",function(){
-    let hasChild = sidebarList.querySelector("#inputLife2_lielem");
+    let hasChild = sidebarListLife.querySelector("#inputLife2_lielem");
     if(hasChild != null){
-        sidebarList.removeChild(hasChild)
+        sidebarListLife.removeChild(hasChild)
     }   
     if(inputLife2.value !== ""){
         let liElement = document.createElement("li")
         liElement.innerText = "LiFE | "+ inputLife2.value
         liElement.id = "inputLife2_lielem";
-        sidebarList.appendChild(liElement);
+        sidebarListLife.appendChild(liElement);
     } 
 });
 
 let inputLife3 = document.getElementById("inputLife3")
 inputLife3.addEventListener("focusout",function(){
-    let hasChild = sidebarList.querySelector("#inputLife3_lielem");
+    let hasChild = sidebarListLife.querySelector("#inputLife3_lielem");
     if(hasChild != null){
-        sidebarList.removeChild(hasChild)
+        sidebarListLife.removeChild(hasChild)
     }   
     if(inputLife3.value !== ""){
         let liElement = document.createElement("li")
         liElement.innerText = "LiFE | "+ inputLife3.value
         liElement.id = "inputLife3_lielem";
-        sidebarList.appendChild(liElement);
+        sidebarListLife.appendChild(liElement);
     } 
 });
 
